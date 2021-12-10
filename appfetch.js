22b8bc3b223e51e7e8959cfe4a6fc49c37017e13
nayru <nanualexandraramirez@gmail.com>
2021-12-10 08:12:40 -0500

    first commit

---
+++ appfetch.js
@@ -0,0 +1,64 @@
+
+
+let numberPage = 1
+const URL =`https://rickandmortyapi.com/api/character/?page=${numberPage}`
+fetch(URL)
+.then(response => response.json())
+.then(data => allthings(data))
+
+function allthings(data){
+    const main= document.querySelector('#main');
+    const contenedor = document.querySelector('#contenedor_card')
+           
+    const fragment = document.createDocumentFragment()
+
+
+    arrays = Object.values(data.results)
+            
+            console.log(arrays)
+            
+        
+            create_card();
+            
+            function create_card(){
+                for (numberPage; numberPage < data.info.pages; numberPage++) {
+                    arrays.forEach(characters =>{
+
+                        const card=document.createElement('div');
+                        const card_icon=document.createElement('div');
+                        const card_info=document.createElement('div');
+                        const tittle_card=document.createElement('h3');
+                        const img_card=document.createElement('img');
+                        const p_card=document.createElement('p');
+                        const p_card_info=document.createElement('p');
+                        const btn_card=document.createElement('button');
+                
+                        card.classList.add('card');
+                        card.setAttribute('id','card');
+                        card_icon.classList.add('icon');
+                        card_icon.setAttribute('id', 'icon');
+                        card_info.classList.add('info_card');
+                        card_info.setAttribute('id', 'info_card')
+                        tittle_card.setAttribute('id','titlle');
+                        tittle_card.textContent=characters.name;
+                        img_card.setAttribute('id', 'image')
+                        img_card.setAttribute('src', characters.image);
+                        p_card.classList.add('precio')
+                        p_card.setAttribute('id', 'parrafo')
+                        p_card.textContent=`Status: ${characters.status} Species:${characters.species}`;
+                        p_card_info.setAttribute('id' ,'info_parrafo');
+                
+                        card.appendChild(card_icon);
+                        card_icon.appendChild(img_card);
+                        card.appendChild(card_info);
+                        card_info.appendChild(tittle_card);
+                        card_info.appendChild(p_card_info)
+                        card_info.appendChild(p_card);
+                        fragment.appendChild(card);
+                    })
+                        contenedor.appendChild(fragment)
+                        main.appendChild(contenedor);
+                }
+                }
+               
+}
